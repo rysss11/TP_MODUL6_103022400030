34111ea5a3bb1f34a88e6a414009aa60dbdb2960
Raissa <talanipanabilah@gmail.com>
2026-04-05 18:42:01 +0700

    menambahkan class SayaMusicTrack

--- Program.cs
+++ Program.cs
@@ -1,1 +1,44 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class SayaMusicTrack
+{
+    private int id;
+    private string playCount;
+    private string title;
+
+    public SayaMusicTrack(string title)
+    {
+        Random random = new Random();
+        this.id = random.Next(00000,100000);
+        this.title = title;
+        this.playCount = "0";
+    } 
+
+    public void IncreasePlayCount(int count)
+    {
+        int current = int.Parse(playCount);
+        current = count++;
+        playCount = current.ToString();
+    }
+
+    public void PrintTrackDetails()
+    {
+        Console.WriteLine("Track Id: " + id);
+        Console.WriteLine("Track Title: " + title);
+        Console.WriteLine("Play Count: " + playCount);
+        Console.WriteLine("");
+    }
+}
+
+public class program
+{
+    static void Main(string[] args)
+    {
+        SayaMusicTrack lagu = new SayaMusicTrack("Lagu 1");
+        SayaMusicTrack lagu2 = new SayaMusicTrack("Lagu 2");
+        lagu.IncreasePlayCount(100);
+        lagu.PrintTrackDetails(); 
+        lagu2.IncreasePlayCount(200);
+        lagu2.PrintTrackDetails();
+    }
+}
--- obj/Debug/net10.0/TP_MODUL6_103022400030.AssemblyInfo.cs
+++ obj/Debug/net10.0/TP_MODUL6_103022400030.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL6_103022400030")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ce40fdc729277fcbc42eb9b42d1aa7c3b376633")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL6_103022400030")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL6_103022400030")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
